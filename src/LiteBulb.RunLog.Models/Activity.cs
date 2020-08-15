e121e914ff1f2d371c0973503e43ef32f381c80c
John Bianchi <jbianchi@starbucks.com>
2020-08-15 16:14:05 -0700

    Made Activity PositionCount property nullable because we only use it when we retrieve the full list of Activity objects in the database (we count them instead of map each Position child object to the parent).  In other words we don't put this value in the database ever.

--- src/LiteBulb.RunLog.Models/Activity.cs
+++ src/LiteBulb.RunLog.Models/Activity.cs
@@ -61,8 +61,9 @@ namespace LiteBulb.RunLog.Models
 		/// Note: this is an optimization property so we don't have to map Position children to each Activity,
 		/// but we can still display the count.  This will be used when retrieving a list of Activities,
 		/// but not when getting a single Activity.
+		/// TODO: fix this later with ViewModel.
 		/// </summary>
-		public int PositionCount { get; set; }
+		public int? PositionCount { get; set; }
 
 		public Activity()
 		{
@@ -76,7 +77,7 @@ namespace LiteBulb.RunLog.Models
 			Description = string.Empty;
 			Units = DistanceUnits.Undefined;
 			Positions = new List<Position>();
-			PositionCount = int.MinValue;
+			PositionCount = null;
 		}
 	}
 }
