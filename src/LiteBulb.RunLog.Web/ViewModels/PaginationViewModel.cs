6d2d1ac084f21b06fe1aceea87c7a41d43452d46
John Bianchi <jbianchi@starbucks.com>
2020-09-06 14:22:13 -0700

    Added a Blazor component to contain much of the Pagination code.

---
+++ src/LiteBulb.RunLog.Web/ViewModels/PaginationViewModel.cs
@@ -0,0 +1,23 @@
+﻿
+namespace LiteBulb.RunLog.Web.ViewModels
+{
+	public class PaginationViewModel
+	{
+		public int PageOffset { get; set; }
+		public int PageSize { get; set; }
+		public long TotalEntityCount { get; set; }
+		public long CurrentEntityCount { get; set; }
+		public long CurrentEntityIndex { get; set; }
+		//public int TotalPageCount { get; set; }
+		//public int TotalPageIndex { get; set; }
+
+		public PaginationViewModel()
+		{
+			PageOffset = 0;
+			PageSize = 10;
+			TotalEntityCount = 0;
+			CurrentEntityCount = 0;
+			CurrentEntityIndex = 0;
+		}
+	}
+}
