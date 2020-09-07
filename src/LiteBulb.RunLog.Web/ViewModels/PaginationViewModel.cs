d91b4cd9ef7d9be21ad2c232abd8a9bf5b6832de
John Bianchi <jbianchi@starbucks.com>
2020-09-06 21:08:18 -0700

    Renamed PageIndex property to PageOffset.

--- src/LiteBulb.RunLog.Web/ViewModels/PaginationViewModel.cs
+++ src/LiteBulb.RunLog.Web/ViewModels/PaginationViewModel.cs
@@ -3,17 +3,17 @@ namespace LiteBulb.RunLog.Web.ViewModels
 {
 	public class PaginationViewModel
 	{
-		public int PageIndex { get; set; }
+		public int PageOffset { get; set; }
 		public int PageSize { get; set; }
 		public long TotalEntityCount { get; set; }
 		public long CurrentEntityCount { get; set; }
 		public long CurrentEntityIndex { get; set; }
 		//public int TotalPageCount { get; set; }
-		//public int TotalPageIndex { get; set; }
+		//public int CurrentPageIndex { get; set; }
 
 		public PaginationViewModel()
 		{
-			PageIndex = 0;
+			PageOffset = 0;
 			PageSize = 10;
 			TotalEntityCount = 0;
 			CurrentEntityCount = 0;
